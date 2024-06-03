315de939d1c7497bebd32de5b1cc706256b017ab
yidnekachew <yidnekachew.esmail@gmail.com>
2024-06-03 16:01:31 +0300

    confused

---
+++ src/main/java/com/example/nameChangedPipelineChatGpt/NameChangedPipelineChatGptApplication.java
@@ -0,0 +1,13 @@
+package com.example.nameChangedPipelineChatGpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameChangedPipelineChatGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameChangedPipelineChatGptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nameChangedPipelineChatGpt/NameChangedPipelineChatGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nameChangedPipelineChatGpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameChangedPipelineChatGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
