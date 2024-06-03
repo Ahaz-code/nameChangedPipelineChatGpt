a69f579ec73bd96325ed32353b99b2af8b8cdf11
yidnekachew <yidnekachew.esmail@gmail.com>
2024-06-03 17:19:32 +0300

    Added docker configuration

--- src/main/java/com/example/nameChangedPipelineChatGpt/NameChangedPipelineChatGptApplication.java
+++ src/main/java/com/example/nameChangedPipelineChatGpt/NameChangedPipelineChatGptApplication.java
@@ -2,10 +2,17 @@ package com.example.nameChangedPipelineChatGpt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class NameChangedPipelineChatGptApplication {
 
+	@GetMapping("/htllo")
+	public String helloWorld(){
+		return "hello world";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(NameChangedPipelineChatGptApplication.class, args);
 	}
